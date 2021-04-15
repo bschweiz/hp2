4c273f01ede979c35923e7693f8aa74f62619ff8
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-14 20:51:10 -0500

    export default statement

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,12 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const NavBar = () => {
+    return (
+
+    )
+}
+
+export default NavBar;
