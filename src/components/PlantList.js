2be5b3b56b992a4cc21f2762b0aee68596e3c1e2
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-15 09:28:18 -0500

    test list banner label printing, need to create card elements to arrange in a grid

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,33 +1,22 @@
 import React, {Component} from 'react'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
-import Plant from '../components/Plant'
+
 
 export const PlantList = (props) => {
-    const { plants, getPlantData } = useContext(PlantContext)
-
-    const [ filteredPlants, setFiltered ] = useState([])
-
-    useEffect(() => {
-        getPlantData()
-    }, [])
-
-    useEffect (()=> {
-        const subset = plants.filter(p => p.userId === parseInt(localStorage.getItem("app_user_id")))
-        setFiltered(subset)
-    }, [plants])
 
     return (
         <>
         <div className="plant__list">
-            <h1>Your Plants</h1>
+            Plant Test List
+            {/* <h1>Your Plants</h1>
             <div className="plants">
                 {
                     shuffle(filteredPlants).map(p => {
                         return <PlantCard key={p.id} plant={p} props={props} />
                     })
                 }
-            </div>
+            </div> */}
         </div>
         </>
     )
